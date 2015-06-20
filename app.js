1dca1b785b3ab0af3e6288a76374d3c8922a6f04
Andrew Tait <andrew.tait@sage.com>
2015-06-20 18:01:13 +0100

    simplified package and added debug script

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/spotify-app');
 
 var routes = require('./routes/index');
 var app = express();
@@ -16,19 +11,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Make our db accessible to our router
-app.use(function(req, res, next){
-  req.db = db;
-  next();
-});
 
 app.use('/', routes);
 
