8e03e607d1bc33cb585821eb1856488ba3583882
Andrew Tait <andrew.tait@sage.com>
2015-06-22 12:06:22 +0100

    fixed coverage

--- test/components/find_artists_component-test.js
+++ test/components/find_artists_component-test.js
@@ -30,16 +30,43 @@ describe('Find Artists Component', function() {
     });
 
     it('calls an ajax request to spotify with the query', function() {
+      this.component.findArtists('foobar');
+      expect(requests[0].url).to.eql('https://api.spotify.com/v1/search?q=foobar&type=artist');
     });
 
     it('sets the state with the data returned', function() {
+      sinon.spy(this.component, 'setState'); // spy here so SearchResults component doesn't update
+
       this.component.findArtists('foobar');
+
       requests[0].respond(
         200,
         "",
-        JSON.stringify({ artists: { items: ['foo'] }})
+        JSON.stringify({ artists: { items: [{
+          name: 'foo',
+          external_urls: { spotify: 'url' },
+          images: []
+        }] }})
       );
-      expect(this.component.state.results).to.eql(['foo']);
+
+      expect(this.component.state.results).to.eql([{
+          name: 'foo',
+          external_urls: { spotify: 'url' },
+          images: []
+        }]);
+    });
+
+    it('fails without setting the state', function() {
+      this.component.state.results = ['bar'];
+      this.component.findArtists('foobar');
+
+      requests[0].respond(
+        400,
+        "",
+        ""
+      );
+
+      expect(this.component.state.results).to.eql(['bar']);
     });
   });
 
